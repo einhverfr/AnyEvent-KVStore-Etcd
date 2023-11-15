335744c5d137808b9f614c3c404381deb0d153df
Chris Travers <chris.travers@gmail.com>
2023-11-15 13:42:52 +0700

    Initial boilerplate from module-starter

---
+++ lib/AnyEvent/KVStore/Etcd.pm
@@ -0,0 +1,105 @@
+package AnyEvent::KVStore::Etcd;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+AnyEvent::KVStore::Etcd - The great new AnyEvent::KVStore::Etcd!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use AnyEvent::KVStore::Etcd;
+
+    my $foo = AnyEvent::KVStore::Etcd->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Chris Travers, C<< <chris.travers at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-anyevent-kvstore-etcd at rt.cpan.org>, or through
+the web interface at L<https://rt.cpan.org/NoAuth/ReportBug.html?Queue=AnyEvent-KVStore-Etcd>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc AnyEvent::KVStore::Etcd
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<https://rt.cpan.org/NoAuth/Bugs.html?Dist=AnyEvent-KVStore-Etcd>
+
+=item * CPAN Ratings
+
+L<https://cpanratings.perl.org/d/AnyEvent-KVStore-Etcd>
+
+=item * Search CPAN
+
+L<https://metacpan.org/release/AnyEvent-KVStore-Etcd>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+This software is Copyright (c) 2023 by Chris Travers.
+
+This is free software, licensed under:
+
+  The (three-clause) BSD License
+
+
+=cut
+
+1; # End of AnyEvent::KVStore::Etcd
